413a59e1e146b99d4cd03ee961349742bdb04f56
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-12-08 17:56:58 +0100

    add carousel pagination

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -1,22 +1,28 @@
 "use client";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules"; // Import Pagination module
 import "swiper/css";
+import "swiper/css/pagination"; // Import Pagination styles
 import gsap from "gsap";
 import { slides } from "@/lib/constants";
 import Image from "next/image";
+import Header1 from "./Header1";
 
 const Carousel = () => {
   return (
     <Swiper
-      modules={[Autoplay]}
+      modules={[Autoplay, Pagination]} // Add Pagination module
       spaceBetween={30}
       slidesPerView={1}
       loop={true}
       autoplay={{
-        delay: 3000, // Auto-slide every 3 seconds
+        delay: 7000, // Auto-slide every 7 seconds
         disableOnInteraction: false,
+      }}
+      pagination={{
+        clickable: true, // Enable clickable dots
+        el: ".swiper-pagination", // Attach custom pagination container
       }}
       onSlideChange={(swiper) => {
         const currentSlide = swiper.slides[swiper.activeIndex];
@@ -38,16 +44,17 @@ const Carousel = () => {
               layout="fill"
               objectFit="cover"
             />
-            <div className="absolute bg-black/50 top-0 bottom-0 left-0 right-0 content">
+            <div className="absolute bg-black/60 top-0 bottom-0 left-0 right-0 content">
+              <Header1 />
               <div className="relative w-full h-full flex items-center px-[3%] lg:px-[5%]">
                 <div>
-                  <h1 className="text-content max-w-[85%] md:max-w-[65%]">
+                  <h1 className="text-content max-w-[95%] md:max-w-[65%]">
                     {slide.headline}
                   </h1>
                   <p className="mt-2 text-content max-w-[75%] md:max-w-[50%] opacity-90">
                     {slide.description}
                   </p>
-                  <button className="mt-8 px-6 py-3 bg-lightBg text-black rounded-full text-content">
+                  <button className="mt-8 px-6 py-3 bg-lightBg font-semibold text-black rounded-full text-content">
                     {slide.cta}
                   </button>
                 </div>
@@ -56,6 +63,8 @@ const Carousel = () => {
           </div>
         </SwiperSlide>
       ))}
+      {/* Add Pagination Container */}
+      <div className="swiper-pagination !bottom-10"></div>
     </Swiper>
   );
 };
---
+++ components/Header1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header1 = () => {
+  return (
+    <nav>
+      <h2>Venture Consult</h2>
+    </nav>
+  )
+}
+
+export default Header1
