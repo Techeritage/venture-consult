a5c66d8e6128a6c97f2a6717a5c6ffba7e348d8a
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2024-12-08 19:17:02 +0100

    fix text error

--- components/About.jsx
+++ components/About.jsx
@@ -14,15 +14,15 @@ const About = () => {
         Welcome to Venture Consult, a leading agency specializing in tax
         consultancy and compliance certification services. We are dedicated to
         helping businesses stay compliant, meet their regulatory obligations,
-        and ensure their operations run smoothly in today's dynamic economic
-        landscape. At Venture Consult, we provide tailored support to help
-        businesses across industries navigate the complexities of tax laws and
-        compliance requirements. From obtaining essential compliance
+        and ensure their operations run smoothly in today&apos;s dynamic
+        economic landscape. At Venture Consult, we provide tailored support to
+        help businesses across industries navigate the complexities of tax laws
+        and compliance requirements. From obtaining essential compliance
         certificates to ensuring seamless tax auditing processes, we are here to
         streamline your journey and deliver expert guidance. Our experienced
         professionals combine deep industry knowledge with a commitment to
-        excellence, ensuring that your business's financial operations are in
-        trusted hands.
+        excellence, ensuring that your business&apos;s financial operations are
+        in trusted hands.
       </p>
     </section>
   );
